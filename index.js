be4548abc65821e47a05ad3738ea4a3a81c58ca0
Marc Grupe <marc.grupe@gmail.com>
2025-03-28 14:18:49 -0400

    init files

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const state = {
 /** Moves a sheep from start to target */
 function moveSheep() {
   // TODO
+  state.target.push(state.start[0]);
+  state.start.pop();
 }
 
 // === Render ===
@@ -21,7 +23,10 @@ function renderStartSheep() {
     li.append(button);
 
     // TODO: Add event listener so the sheep moves when clicked
-
+    button.addEventListener("click", (event) => {
+      moveSheep();
+      render();
+    })
     return li;
   });
 
@@ -32,6 +37,17 @@ function renderStartSheep() {
 /** Renders sheep on the target bank */
 function renderTargetSheep() {
   // TODO
+   const targetSheep = state.target.map((sheep) => {
+     const li = document.createElement("li");
+
+     const button = document.createElement("button");
+     button.textContent = "🐑";
+     li.append(button);
+     return li;
+   });
+
+   const targetBank = document.querySelector("#targetBank ul");
+   targetBank.replaceChildren(...targetSheep);
 }
 
 function render() {
@@ -44,3 +60,14 @@ function render() {
 render();
 
 // TODO: Add sheep to the starting bank when the form is submitted
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const numSheepToAdd = data.get("numSheep");
+  console.log(numSheepToAdd);
+  for (let i = 0; i < numSheepToAdd; i++){
+    state.start.push("sheep");
+  }
+  render();
+})
